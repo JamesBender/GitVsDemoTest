61908470dc464940dae8ee5ad7a71d87a3678892
James Bender <James@JamesCBender.com>
2014-08-27 20:10:51 -0400

    Added some code for feature two

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //This is code for feature One
+
+            //This is code for feature Two
         }
     }
 }
